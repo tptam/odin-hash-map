54817026a2f40b71bd2cb68f2567dad1283759c3
tptam <99264397+tptam@users.noreply.github.com>
2024-08-04 23:52:28 +0900

    Add hash(key)

---
+++ hash-map.js
@@ -0,0 +1,14 @@
+class HashMap {
+  #capacity = 16;
+  #loadFactor = 0.8;
+  #buckets;
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+    return hashCode;
+  }
+}
