5215a43343a193ae1ec323159da1f9f47d68dba4
tptam <99264397+tptam@users.noreply.github.com>
2024-08-05 01:33:59 +0900

    Implement values()

--- hash-map.js
+++ hash-map.js
@@ -86,6 +86,18 @@ class HashMap {
     return result;
   }
 
+  values() {
+    const result = [];
+    this.#buckets.forEach((bucket) => {
+      let current = bucket.head();
+      while (current !== undefined && current !== null) {
+        result.push(current.value);
+        current = current.nextNode;
+      }
+    });
+    return result;
+  }
+
   toString() {
     let string = "";
     this.#buckets.forEach((bucket, index) => {
